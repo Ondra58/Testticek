ac83c89a50122d9e101db8faab96b816ee6243e5
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-10-02 11:01:39 +0200

    Add files via upload

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vypočítat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(58, 302);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(214, 302);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Příklady";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Vypočítané příklady";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+                StreamReader ctenar = new StreamReader("matematika.txt");
+                while (!ctenar.EndOfStream)
+                {
+                    listBox1.Items.Add(ctenar.ReadLine());
+                }
+                int[] vysledek = new int[5];
+                int i = 0;
+                /*ctenar.BaseStream.Seek(0, SeekOrigin.Begin);
+                while (!ctenar.EndOfStream)
+                {
+                    string priklad = ctenar.ReadLine();
+                    int cislo1 = priklad[0] - 48;
+                    char operand = priklad[1]; 
+                    int cislo2 = priklad[2] - 48;
+                    MessageBox.Show(operand.ToString());
+                }*/
+                string[] priklady = new string[5];
+                foreach (string priklad in listBox1.Items)
+                {
+                    priklady[i] = priklad;
+                    priklad.Split(' ');
+                    int cislo1 = priklad[0] - 48;
+                    char operand = priklad[2];
+                    int cislo2 = priklad[4] - 48;
+                    switch (operand)
+                    {
+                        case '+':
+                            {
+                                vysledek[i] = cislo1 + cislo2;
+                                break;
+                            }
+                        case '-':
+                            {
+                                vysledek[i] = cislo1 - cislo2;
+                                break;
+                            }
+                        case '*':
+                            {
+                                vysledek[i] = cislo1 * cislo2;
+                                break;
+                            }
+                        case '/':
+                            {
+                                vysledek[i] = cislo1 / cislo2;
+                                break;
+                            }
+                        case '%':
+                            {
+                                vysledek[i] = cislo1 % cislo2;
+                                break;
+                            }
+                    }
+                    i++;
+                }
+                ctenar.Close();
+                i = 0;
+                int soucet = 0;
+                StreamWriter zapisovak = new StreamWriter("matematika.txt", false);
+                foreach (int cislo in vysledek)
+                {
+                    zapisovak.WriteLine(priklady[i] + " " + cislo);
+                    listBox2.Items.Add(priklady[i] + " " + cislo);
+                    soucet += cislo;
+                    i++;
+                }
+                zapisovak.Close();
+                double prumer = (double)soucet / i;
+                FileStream datovytok = new FileStream("prumer.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                BinaryWriter zapisovak2 = new BinaryWriter(datovytok);
+                zapisovak2.Write(prumer);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (StackOverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
